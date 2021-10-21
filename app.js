4e785791c6e762543c300f225421601f4b376de3
akshay1431 <akshay.ramya@gmail.com>
2021-10-21 11:30:32 +0530

    import Js

---
+++ app.js
@@ -0,0 +1,11 @@
+//console.log("Hello script is working")
+
+//alert("this script works")
+
+
+var username = prompt("Enter your name")
+
+var welcomemessage = "welcome" +username;
+
+alert(welcomemessage)
+
